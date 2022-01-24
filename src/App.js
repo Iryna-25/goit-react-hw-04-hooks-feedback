cbc697e28829203ba7f69809b4196b9e757afce0
Iryna Shandar <iryna.shandar@gmail.com>
2022-01-24 23:49:43 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
-// App.js
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
+import FeedbackOptions from './Components/FeedbackOptions';
+import Statistic from './Components/Statistics';
+import Notification from './Components/Notification';
 
-function App() {
-  return <div className="App">TEST</div>;
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = (good * 100) / countTotalFeedback;
+
+  const handleIncrement = button => {
+    switch (button.target.id) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Please leave feedback</h1>
+      <FeedbackOptions onLeaveFeedback={handleIncrement} good={good} neutral={neutral} bad={bad} />
+      <h2>Statistics</h2>
+      {countTotalFeedback > 0 ? (
+        <Statistic
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          countTotalFeedback={countTotalFeedback}
+          countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
 }
 
-export default App;
+App.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  countTotalFeedback: PropTypes.number,
+  countPositiveFeedbackPercentage: PropTypes.number,
+  onIncrement: PropTypes.func,
+};
